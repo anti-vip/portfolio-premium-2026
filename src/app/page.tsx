22e4bedac2985e18c997b6e19a6197123c3f46b6
anti <naelkhannouf@gmail.com>
2026-04-26 12:28:30 +0200

    Add app source

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { ClientAccess } from "@/components/client-access";
+import { HeroSection } from "@/components/hero-section";
+import { ProjectBento } from "@/components/project-bento";
+import { SiteNav } from "@/components/site-nav";
+import { getFeaturedProjects } from "@/lib/projects";
+
+export default async function Home() {
+  const projects = await getFeaturedProjects();
+
+  return (
+    <main className="relative min-h-screen overflow-hidden">
+      <SiteNav />
+      <HeroSection />
+      <ProjectBento projects={projects} />
+      <ClientAccess />
+    </main>
+  );
+}
